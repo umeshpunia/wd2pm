360779627d67101b10e4e94fe1f09a79e07de1cd
Umesh Singh Punia <punia.umesh@gmail.com>
2022-05-09 14:52:00 +0530

    Add files via upload

--- js/first.js
+++ js/first.js
@@ -1,6 +1,76 @@
-var a=9
-b=2
+var a = 9;
+b = 2;
+
+// 1. using 3rd/temp variable
+
+// c=a
+// a=b
+// b=c
+
+// 2. without using 3rd
+// a=a+b
+// b=a-b
+// a=a-b
+
+// 3. single line
+
+// a=(a+b)-(b=a)
+
+// document.write("the value of a="+a+"<br>the value of b="+b)
+// document.write(a==9)
+
+// a = 20;
+// b = 20;
 
 
 
-document.write(!true)
+// month=new Date().getMonth()
+
+// switch (month) {
+//   case 0:
+//     document.write("B");
+//     break;
+//   case 4:
+//     document.write("May");
+//     break;
+//   default:
+//     document.write("Eql");
+// }
+
+// if(a>b){
+//     document.write("A")
+// }else if(b>a){
+//     document.write("B")
+// }else{
+//     document.write("Equal")
+// }
+
+
+
+
+
+// 0-400
+
+for(
+    i=0; //i=21
+    i<=20; //21<=20 - f
+    i++
+){
+    document.write(i+"<br>")
+}
+
+
+// 5*1=5
+
+
+
+
+
+
+
+
+
+
+
+
+
