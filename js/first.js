170cb8d9faa6c63406913e684324e40d1f079244
Umesh Singh Punia <punia.umesh@gmail.com>
2022-05-10 15:01:48 +0530

    Add files via upload

--- js/first.js
+++ js/first.js
@@ -51,24 +51,69 @@ b = 2;
 
 // 0-400
 
-for(
-    i=0; //i=21
-    i<=20; //21<=20 - f
-    i++
-){
-    document.write(i+"<br>")
-}
+// for(
+//     i=0; //i=21
+//     i<=20; //21<=20 - f
+//     i++
+// ){
+//     document.write(i+"<br>")
+// }
 
 
 // 5*1=5
 
 
+// table=20
+
+// for(i=1;i<=10;i++){
+//     document.write(table+"*"+i+"="+table*i+"<br>")
+// }
+
+
+// a=10
+// b=10
+// c=3
+// d=4
+
+
+// switch(true)
+// {
+
+//     case a>b && a>c && a>d:
+//         document.write("A")
+//         break
+
+//     default:
+//         document.write("DS")
+
+
+// }
 
 
 
 
 
 
+// 15-5
+// a=15
+// do{
+//     document.write(a+"<br>")
+//     a--
+// }while(a>=50)
+
+
+for(i=0;i<5;i++){
+    for(j=0;j<5;j++){
+       if(i==0 || i==2){
+           document.write("*")
+       }else if(j==0 || j==4){
+           document.write("*")   
+       }else{
+           document.write("&nbsp;&nbsp;")
+       }
+    }
+    document.write("<br>")
+}
 
 
 
